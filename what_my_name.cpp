aafa580f2858d30f80bc0cae2144fb696164075c
US Samrat <ksamrat629@gmail.com>
2025-10-01 12:29:51 +0530

    Added new File

---
+++ what_my_name.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string name;
+    cout<<"Enter your name: "<<endl;
+    cin>>name;
+    cout<<"your name is: "<<name<<endl;
+    return 0;
+}
